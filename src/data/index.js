40bf4119415ad78a8cd85f612ffc257956714ccc
Amit Kumar <amit.engg9897@gmail.com>
2023-06-20 17:46:46 +0530

    frist

---
+++ src/data/index.js
@@ -0,0 +1,19 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Agrima',
+    lastName: 'Agarwal',
+    email: 'agrima@example.com',
+    phonenumber: '6355443256',
+  },
+  {
+    id: 2,
+    firstName: 'Nikita',
+    lastName: 'Singh',
+    email: 'nikita@example.com',
+    phonenumber: '5522338877',
+  },
+  
+];
+
+export { employeesData };
